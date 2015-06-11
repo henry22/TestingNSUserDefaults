96cd10d25aafd5710d9031aa10c71a7f3a2f7500
henry22 <henry811010@gmail.com>
2015-06-11 21:57:12 +0800

    Initial Commit

---
+++ TestingNSUserDefaults/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestingNSUserDefaults
+//
+//  Created by Henry on 6/11/15.
+//  Copyright (c) 2015 Henry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
