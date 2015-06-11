6bf12c5291d2058c66b692c71980fb1a8bb8eac4
henry22 <henry811010@gmail.com>
2015-06-11 22:39:07 +0800

    Testing code.

--- TestingNSUserDefaults/ViewController.swift
+++ TestingNSUserDefaults/ViewController.swift
@@ -12,7 +12,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        //Just need to give each one a unique key so we can reference it later
+        defaults.setInteger(22, forKey: "Age")
+        defaults.setBool(true, forKey: "UseTouchID")
+        defaults.setDouble(M_PI, forKey: "Pi")
+        
+        //Use the setObject() to set strings, arrays, dictionaries and dates
+        defaults.setObject("Henry Chien", forKey: "Name")
+        defaults.setObject(NSDate(), forKey: "LastRun")
+        
+        let array = ["Hello", "World"]
+        defaults.setObject(array, forKey: "SavedArray")
+        
+        let dict = ["Name": "Henry", "Country": "Taiwan"]
+        defaults.setObject(dict, forKey: "SavedDict")
+        
+        //Use objectForKey() and as? to get an optional object, then use ?? to either unwrap the object or set a default value
+        let loadedArray = defaults.objectForKey("SavedArray") as? [String] ?? [String]()
+        let loadedDict = defaults.objectForKey("SavedDict") as? [String: String] ?? [String: String]()
     }
 
     override func didReceiveMemoryWarning() {
